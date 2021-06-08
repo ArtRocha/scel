2cb90daebf236c4b706628a5c257545340b9888d
Giulia Ventura Ratti <52727860+VenturaGiu@users.noreply.github.com>
2021-06-07 23:18:02 -0300

    Delete scc directory

--- scc/src/main/java/com/fatec/scc/model/AdvogadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fatec.scc.model;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdvogadoRepository extends CrudRepository<Advogado, Long> {
-	public Advogado findByCpf(@Param("isbn") String cpf);
-}
